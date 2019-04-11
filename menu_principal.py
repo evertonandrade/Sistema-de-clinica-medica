810dd61f2e77d227d2d50e7b5e823483b377bed8
= <=>
2019-04-11 10:32:51 -0300

    formatação de string

--- menu_principal.py
+++ menu_principal.py
@@ -3,10 +3,11 @@ import medicos as md
 import consultas as ct
 import administracao as adm
 
-" Author: Everton Andrade "
+" Author: Ian Sandes "
 
 
 def menu_principal():
+    print("\n")
     print(20 * "-=")
     print("       SISTEMA DE CLÍNICA MÉDICA")
     print(20 * "-=")
@@ -16,25 +17,20 @@ def menu_principal():
           "2 - Médicos\n"
           "3 - Consultas\n"
           "4 - Administrativo\n"
-          "5 - Fechar\n"
           "--------------------")
 
     while True:
         atendimento = int(input("Escolha o tipo de atendimento a ser usado: "))
         if atendimento == 1:
             pt.menu_pacientes()
-        if atendimento == 2:
+        elif atendimento == 2:
             md.menu_medicos()
-        if atendimento == 3:
+        elif atendimento == 3:
             ct.menu_consultas()
-        if atendimento == 4:
+        elif atendimento == 4:
             adm.menu_administracao()
-        if atendimento == 5:
-            break
         else:
             print("Opção inválida!\n")
-
-
 
 
 menu_principal()
